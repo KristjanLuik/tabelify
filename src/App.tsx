78fe6f2da43fe7e42d343cb248b06af7804e4f95
kristjanluik <kristjanluik1@gmail.com>
2024-04-30 20:51:57 +0300

    testing env

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import './App.css'
+// import ReactGA from 'react-ga';
+
 import MainScene from "./components/MainScene";
 import TextureMapper from "./components/TextureMapper";
 import Logo from "./components/Logo";
 import { AppProvider } from "./appContext.tsx";
 
+import './App.css'
+
+
+
 function App() {
+//const TRACKING_ID = "G-62FR2ML7Q9";
+//ReactGA.initialize(TRACKING_ID);
 
   return (
     <>
         <AppProvider>
             <Logo/>
+            <span>Su tag{import.meta.env.GTAG}</span>
             <div className="App">
                 <MainScene/>
                 <TextureMapper/>
